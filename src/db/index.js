b23b9e3432300d85777de58f78456fb1a8a76c8f
unknown <nhatduy1284t@gmail.com>
2022-04-22 11:36:43 +0700

    add git ignore

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const express = require("express")
 mongoose.connects = async function () {
   try {
-    const app= express();
-    app.use(express.json())
-    await mongoose.connect('mongodb://localhost:27017/mesh', { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect('mongodb://localhost:27017/mesh');
     console.log('Connect successfully!');
   } catch (error) {
     console.log('Connect fail!');
